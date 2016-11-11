4f489af36a91e0ef78a6a792ba96a1e2ddf72369
abhilashkasap <abhilashnco@gmail.com>
2016-11-11 22:51:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -210,7 +210,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
     }
 });
 
-app.get('/articles/:articleName', function (req, res) {
+/*app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
   pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
     if (err) {
@@ -221,6 +221,21 @@ app.get('/articles/:articleName', function (req, res) {
         } else {
             var articleData = result.rows[0];
             res.send(createTemplate(articleData));
+        }
+    }
+  });
+});*/
+app.get('/articles/:articleName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(JSON.stringify(result.rows));
         }
     }
   });
