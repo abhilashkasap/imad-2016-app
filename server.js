b0589c0288368a3e267722679e90d19d04c68181
abhilashkasap <abhilashnco@gmail.com>
2016-11-12 00:29:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ function createTemplate (data) {
     var content = data.content;
     
     var htmlTemplate = `
- <html>
+ <!DOCTYPE html>
 <head>
     <title>
         ${title}
@@ -202,7 +202,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
     }
 });
 
-/*app.get('/articles/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
   pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
     if (err) {
@@ -216,22 +216,8 @@ app.post('/submit-comment/:articleName', function (req, res) {
         }
     }
   });
-});*/
-app.get('/articles/:articleName', function (req, res) {
-  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
-    if (err) {
-        res.status(500).send(err.toString());
-    } else {
-        if (result.rows.length === 0) {
-            res.status(404).send('Article not found');
-        } else {
-            var articleData = result.rows[0];
-            res.send(JSON.stringify(result.rows));
-        }
-    }
-  });
 });
+
 
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
